418dff8cd6396b170d0a0965f31bc5b92b2dff08
prePhilip <85646938+prePhilip@users.noreply.github.com>
2021-06-25 20:02:53 -0500

    Update script.js

--- Mod4-assgn/script.js
+++ Mod4-assgn/script.js
@@ -57,7 +57,7 @@ WARNING!!! WARNING!!!
 	var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 	
 	for (var i = 0; i < names.length ; i++) {
-		if((names[i].charAt(0) === "J") || ((names[i].charAt(0)).toLowerCase  === "j")) {
+		if((names[i].charAt(0) === "j") || ((names[i].charAt(0)).toLowerCase  === "j")) {
 			
 			byeSpeaker.speak(names[i]);
 			
